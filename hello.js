845b87a2e3708a5549cb25735815f9b1eff958b9
coolcom200 <leonfattakhov@yahoo.com>
2017-08-12 03:36:39 -0400

    updated server2

--- hello.js
+++ hello.js
@@ -9,7 +9,7 @@ console.log(parse('last 2 days'));
 var chrono = require('chrono-node');
 console.log(chrono.parseDate('An appointment on Sep 12-13')) ;
 //An appointment on 5 pm Sep 12 to 3 pm Sep 13
-var res = chrono.parse('I am free today after 10:00');
+var res = chrono.parse('I am free today from 9:00 to 10:00 and from 12:00 to 5');
 console.log(res);
 console.log(res[0].start) ;
 console.log(res[0].end) ;
--- server2.js
+++ server2.js
@@ -1,16 +1,31 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
+var chrono = require('chrono-node');
+
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json())
 
 var port = 9000;
 
+
 app.post('/post/data', function(req, res) {
     console.log('receiving data...');
-    console.log('body is ',req.body);
-    res.send(req.body);
+    console.log('body is ', req.body);
+    console.log("\n")
+    var data = req.body["messages"];
+    for (var i = 0; i < data.length; i++) {
+        var parse_results = chrono.parse(data[i]["message"]);
+        for (var j = 0; j < parse_results.length; j++) {
+        	console.log("Start", parse_results[j].start)
+        	console.log("End", parse_results[j].end)
+
+        }
+
+    }
+
 });
 
 // start the server
