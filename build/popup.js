3cff282d230200b13f16a8cc6dae3633d5c3757e
Ethan Guo <ethguo@gmail.com>
2017-08-12 09:15:39 -0400

    set up successHandler

--- build/popup.js
+++ build/popup.js
@@ -1,12 +1,17 @@
 "use strict";
 
+function successHandler(res) {
+  $("#message", res.toString());
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   var tab = tabs[0];
   console.log(tab.url, tab.title);
   chrome.tabs.getSelected(null, function (tab) {
     chrome.tabs.sendMessage(tab.id, { action: "scrape" }, function (req) {
       if (req.action == "scrape") {
-        document.querySelector("#message").innerText = req.messages;
+        $("#message").text(req.messages.length);
+        $.post("127.0.0.1", { "messages": messages }, successHandler);
       }
     });
   });
--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,15 @@
+function successHandler(res) {
+  $("#message", res.toString())
+}
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   var tab = tabs[0]
   console.log(tab.url, tab.title)
   chrome.tabs.getSelected(null, function(tab) {
     chrome.tabs.sendMessage(tab.id, {action: "scrape"}, function(req) {
-      if (req.action=="scrape") {
-        document.querySelector("#message").innerText = req.messages
-        
+      if (req.action == "scrape") {
+        $("#message").text(req.messages.length)
+        $.post("127.0.0.1", {"messages": messages}, successHandler)
       }
     })
   })
