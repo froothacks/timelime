8045c1cf56c3a734c09ea2b56d416f5b1c69a129
Ethan Guo <ethguo@gmail.com>
2017-08-12 17:26:07 -0400

    Forcing remote-master branch to match dev branch

--- build/popup.js
+++ build/popup.js
@@ -1,44 +1,36 @@
 "use strict";
 
 function successHandler(res) {
+  console.log(res);
+  // $("#message").text(JSON.stringify(res))
 
-  alert("in");
-  console.log("in");
-  $("#message", res.toString());
-  console.log(res);
-  console.log(res);
-  $("#main").text(JSON.stringify(res));
+  var container = $("#timeline")[0];
+  var data = new vis.DataSet(res.data);
+  var groups = new vis.DataSet(res.groups);
+  var options = {
+    stack: false,
+    // start: new Date(),
+    // end: new Date(7*24*60*60*1000 + (new Date()).valueOf()),
+    editable: true,
+    margin: {
+      item: 10, // minimal margin between items
+      axis: 5 // minimal margin between items and the axis
+    },
+    orientation: "top"
+  };
+  var timeline = new vis.Timeline(container, data, groups, options);
 }
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   var tab = tabs[0];
   console.log(tab.url, tab.title);
-  console.log("eyo");
-  // alert("out");
   chrome.tabs.getSelected(null, function (tab) {
-    // alert("tab");
     chrome.tabs.sendMessage(tab.id, { action: "scrape" }, function (req) {
-      // alert(req.action);
       if (req.action == "scrape") {
-        $("#message").text(req.messages.length);
-        alert("byyy");
-        // alert(JSON.stringify(messages));
-        // alert("s");
-        $.ajax({
-          type: "POST",
-          url: "http://127.0.0.1:9000/post/data",
-          data: { "messages": req.messages },
-          success: successHandler,
-          error: function (xhr, ajaxOptions, thrownError) {
-            alert(xhr.status);
-            alert(thrownError);
-          },
-          dataType: "json"
-        });
-
-        // $.post("127.0.0.1", { "messages": messages }, successHandler);
-        $("#main").text(req.messages.length);
-        $.post("127.0.0.1:9000/post/data", { "messages": req.messages }, successHandler);
+        // $("#message").text(req.messages.length)
+        var data = { "messages": JSON.stringify(req.messages) };
+        console.log(data);
+        $.post("http://127.0.0.1:9000/post/data", data, successHandler);
       }
     });
   });
--- server.js
+++ server.js
@@ -1,14 +1,64 @@
 const express = require('express');
+// console.log(WtoN.parse("five"));
+// console.log(WtoN.parse("today at five"));
+// console.log(WtoN.parse("three to five"));
+// console.log(WtoN.parse("today from three to five"));
 const bodyParser = require('body-parser');
 const chrono = require('chrono-node');
-const sentiment = require('sentiment');
-const cors = require('cors');
+const sentiment = require('sentiment')
+
 const app = express();
-app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json())
+
+const NAME_TO_NUMBER = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+    "ten": "10",
+    "eleven": "11",
+    "twelve": "12",
+    "thirteen": "13",
+    "fourteen": "14",
+    "fifteen": "15",
+    "sixteen": "16",
+    "seventeen": "17",
+    "eighteen": "18",
+    "nineteen": "19",
+    "twenty": "20",
+    "twenty one": "21",
+    "twenty two": "22",
+    "twenty three": "23",
+    "twenty four": "24",
+    "twenty five": "25",
+    "twenty six": "26",
+    "twenty seven": "27",
+    "twenty eight": "28",
+    "twenty nine": "29",
+    "thirty": "30",
+    "thirty one": "31",
+    "twenty-one": "21",
+    "twenty-two": "22",
+    "twenty-three": "23",
+    "twenty-four": "24",
+    "twenty-five": "25",
+    "twenty-six": "26",
+    "twenty-seven": "27",
+    "twenty-eight": "28",
+    "twenty-nine": "29",
+    "thirty-one": "31"
+
+
+}
 
 const PORT = 9000;
 const OVERRIDES = {
@@ -23,28 +73,24 @@ const OVERRIDES = {
 
 app.post('/post/data', function(req, res) {
     console.log('receiving data...');
-    console.log(JSON.stringify(req.body));
-    var bod = req.body;
-    var data = {"messages":[]};
-    var dataKeys = Object.keys(bod);
+    var data = JSON.parse(req.body["messages"]);
 
-    var results = (dataKeys.length-2)/2;
-    console.log(results);
-    for (var i = 0; i <= results; i += 1) {
-    	console.log(i);
-    	console.log(data);
-    	var temp = {"username":bod["messages[" + i + "][username]"], "message": bod["messages[" + i + "][message]"]};
-    	data["messages"].push(temp);
-    	console.log(data);
-    }
-    console.log(data);
-    // "messages[0]['username']"
-    console.log(req.body["messages"]);
-    var response = {"availability":[]};
+    var response = {
+        "availability": []
+    };
     var userDict = {};
-    var data = data["messages"];
+    var userRanges = {};
     for (var i = 0; i < data.length; i++) {
-        var parse_results = chrono.parse(data[i]["message"]);
+        var message = data[i]["message"].toLowerCase();
+
+        var words_list = data[i]["message"].split(" ");
+        console.log(message.toLowerCase());
+        for (var j = 0; j < words_list.length; j++) {
+            if (Object.keys(NAME_TO_NUMBER).indexOf(words_list[j])!==-1) {
+                message = message.replace(words_list[j], NAME_TO_NUMBER[words_list[j]])
+            }
+        }
+        var parse_results = chrono.parse(message);
         var user_name = data[i]["username"];
         for (var j = 0; j < parse_results.length; j++) {
             var date_dictionary = parse_results[j].start.knownValues;
@@ -53,7 +99,7 @@ app.post('/post/data', function(req, res) {
             for (var ite = 0; ite < keys.length; ite++) {
                 date_dictionary[keys[ite]] = date_dictionary_i[keys[ite]];
             }
-            var startTime = new Date(date_dictionary.year, date_dictionary.month - 1, date_dictionary.day, date_dictionary.hour - 4, date_dictionary.minute, date_dictionary.second);
+            var startTime = new Date(date_dictionary.year, date_dictionary.month - 1, date_dictionary.day, date_dictionary.hour, date_dictionary.minute, date_dictionary.second);
             var endTime;
             if (parse_results[j].end != undefined) {
                 var d = parse_results[j].end.knownValues;
@@ -62,42 +108,64 @@ app.post('/post/data', function(req, res) {
                 for (var iter = 0; iter < _keys.length; iter++) {
                     d[_keys[iter]] = y[_keys[iter]];
                 }
-                endTime = new Date(d.year, d.month - 1, d.day, d.hour - 4, d.minute, d.second);
-            } 
-            else {
-                endTime = new Date(date_dictionary.year, date_dictionary.month - 1, date_dictionary.day, date_dictionary.hour - 4 + 1, date_dictionary.minute, date_dictionary.second);
+                endTime = new Date(d.year, d.month - 1, d.day, d.hour, d.minute, d.second);
+            } else {
+                endTime = new Date(date_dictionary.year, date_dictionary.month - 1, date_dictionary.day, date_dictionary.hour + 1, date_dictionary.minute, date_dictionary.second);
             }
 
             var boolAvailable;
             var message_body = data[i]["message"];
             if (sentiment(message_body, OVERRIDES)["score"] < 0) {
-            	boolAvailable = "false";
+                boolAvailable = "false";
+            } else {
+                boolAvailable = "true";
             }
-            else {
-            	 boolAvailable = "true";
+            var startStamp = startTime.valueOf();
+            var endStamp = endTime.valueOf();
+
+            var details = {
+	                    	"available": boolAvailable,
+	                    	"start": startTime,
+	                    	"end": endTime
+                		};
+            var userKeys = Object.keys(userDict);
+            for (var usernamei = 0; usernamei < userKeys.length; usernamei++) {
+            	for (var rangei = 0; rangei < userDict[userKeys[usernamei]].length; rangei++){
+		    		// console.log(rangei);
+		    		// console.log(userKeys[usernamei]);
+		    		var curSet = userDict[userKeys[usernamei]][rangei];
+
+		    	}
             }
             if (userDict[user_name] === undefined) {
-            	userDict[user_name] = [{"available":boolAvailable, "start":startTime, "end":endTime}];
-            }
-            else {
-            	userDict[user_name].push({"available":boolAvailable, "start":startTime, "end":endTime});
+                userDict[user_name] = [details];
+                // userRange[user_name] = [[details["start"], ]
+            } else {
+                userDict[user_name].push(details);
             }
         }
 
-        console.log("d");
+        console.log("dict:");
         console.log(userDict);
         console.log(response);
+    }
 
-    }
     var userKeys = Object.keys(userDict);
+    var newrep = {"data": [], "groups": []};
+    var counter = 0;
+    console.log(userDict);
     for (var usernamei = 0; usernamei < userKeys.length; usernamei++) {
-    	response["availability"].push({"username":userKeys[usernamei], "times":userDict[userKeys[usernamei]]});
+    	newrep["groups"].push({"id":usernamei, "content":userKeys[usernamei]})
+    	for (var rangei = 0; rangei < userDict[userKeys[usernamei]].length; rangei++){
+    		console.log(rangei);
+    		console.log(userKeys[usernamei]);
+    		var curSet = userDict[userKeys[usernamei]][rangei];
+    		newrep["data"].push({"id":counter, content:"", "start":String(curSet["start"]), "end":String(curSet["end"]), "group":usernamei, "className":((curSet["available"] == "true") ? "available":"busy")});
+    		counter++;
+    	}
     }
-    console.log(response);
-    for (var i = 0; i < response.length; i++){
-    	console.log(response[i]["times"]);
-    }
-    res.send(response);
+    console.log(newrep);
+    res.send(newrep);
 });
 
 // start the server
