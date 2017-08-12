3187153f9a8d96648863f5ad8d3d52451bcc1958
Advait Maybhate <advait.maybhate9@gmail.com>
2017-08-12 12:26:11 -0400

    Fixing POST request

--- build/popup.js
+++ build/popup.js
@@ -1,17 +1,39 @@
 "use strict";
 
 function successHandler(res) {
+  alert("in");
+  console.log("in");
   $("#message", res.toString());
+  console.log(res);
 }
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   var tab = tabs[0];
   console.log(tab.url, tab.title);
+  console.log("eyo");
+  // alert("out");
   chrome.tabs.getSelected(null, function (tab) {
+    // alert("tab");
     chrome.tabs.sendMessage(tab.id, { action: "scrape" }, function (req) {
+      // alert(req.action);
       if (req.action == "scrape") {
         $("#message").text(req.messages.length);
-        $.post("127.0.0.1", { "messages": messages }, successHandler);
+        alert("byyy");
+        // alert(JSON.stringify(messages));
+        // alert("s");
+        $.ajax({
+          type: "POST",
+          url: "http://127.0.0.1:9000/post/data",
+          data: { "messages": req.messages },
+          success: successHandler,
+          error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+          },
+          dataType: "json"
+        });
+
+        // $.post("127.0.0.1", { "messages": messages }, successHandler);
       }
     });
   });
