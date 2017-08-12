a25e0e7b7028ff0754b0138884331984ed10b473
coolcom200 <leonfattakhov@yahoo.com>
2017-08-12 02:44:04 -0400

    Added POST request data getter

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const JSON_example = `cars": {"car1":"Ford","car2":"BMW","car3":"Fiat"}`
 
 
 const server = http.createServer((request, response) => {
-	console.log(request.method);
-	const {body} = request;
-	if (request.method == "POST"){
-		console.log(request.body)
+    console.log(request.method);
+    if (request.method == "POST") {
+        var body = "";
+        request.on('data', function(chunk) {
+            body += chunk;
+        }).on('end', function() {
+            console.log("HERE", body);
+        });
+        console.log(request.body)
         response.statusCode = 200;
         response.setHeader('Content-Type', 'application/json');
         response.end(JSON_example);
