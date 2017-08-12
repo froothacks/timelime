a35c2a2c93da2d2331f2db547bc2087206900bef
Advait Maybhate <advait.maybhate9@gmail.com>
2017-08-12 03:37:46 -0400

    New date constructors

--- hello.js
+++ hello.js
@@ -11,5 +11,8 @@ console.log(chrono.parseDate('An appointment on Sep 12-13')) ;
 //An appointment on 5 pm Sep 12 to 3 pm Sep 13
 var res = chrono.parse('I am free today after 10:00');
 console.log(res);
-console.log(res[0].start) ;
+console.log(res[0].start.knownValues);
+console.log(chrono.parseDate(res[0].start.knownValues));
+var d = res[0].start.knownValues;
+console.log(new Date(d.year, d.month, d.day, d.hour, d.minute, d.second));
 console.log(res[0].end) ;
