527726580c9dc842df3a30b0a7b400087c7ed4da
coolcom200 <leonfattakhov@yahoo.com>
2017-08-12 02:01:31 -0400

    Updated Server.js kinda broken

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const server = http.createServer((request, response) => {
+	console.log(request.method);
+    if (request.method == "GET") {
+    	console.log("In Get");
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end('Hello World\n');
+    } else {
+        var chrono = require('chrono-node');
+        var res = chrono.parse('I am free today after 10:00');
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        response.end(res);
+    }
+
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
